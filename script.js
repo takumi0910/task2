a18dabe35d057d2d97bab7f278028ac13c3cd24f
takumi0910 <takedagen0910@gmail.com>
2020-08-18 11:20:43 +0900

    footerレスポンシブル化完成

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ $(function () {
     $('.contents13').click(function () {
         $(this).children(".inner1").slideToggle(300);
         $(this).children(".accordion1").toggleClass("active");
-        $(this).children(".accordion1").toggleClass("border");
     });
     
   
